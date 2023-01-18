d5fe1e8368fbc65ff1f2cc5530f2e5f91f8ceb9f
Guido Squillace <g.squillace@cantierecreativo.net>
2023-01-18 14:56:49 +0100

    Fix

--- components/scripts/Iubenda.js
+++ components/scripts/Iubenda.js
@@ -5,12 +5,11 @@ export default function Iubenda({ siteId, policyId, visual, locale }) {
   const buttonBackColor = visual.colorButtonBack.hex;
   return (
     <>
-      <Script id="iubenda-cs" src="//cdn.iubenda.com/cs/iubenda_cs.js" />
       <Script
         id="iubenda"
         dangerouslySetInnerHTML={{
           __html: `var _iub = _iub || [];
-            _iub.csConfiguration = {
+          _iub.csConfiguration = {
             "localConsentDomain":"museocivico.comune.siena.it”,
             "consentOnContinuedBrowsing":true,
             "invalidateConsentWithoutLog":true,
@@ -43,6 +42,17 @@ export default function Iubenda({ siteId, policyId, visual, locale }) {
             `,
         }}
       />
+      <Script id="iubenda-stub" src="//cdn.iubenda.com/cs/gpp/stub.js" />
+      <Script id="iubenda-cs" src="//cdn.iubenda.com/cs/iubenda_cs.js" />
     </>
   );
+}
+
+{
+  /* <script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"countryDetection":true,"enableLgpd":true,"enableUspr":true,"floatingPreferencesButtonDisplay":"bottom-right","invalidateConsentWithoutLog":true,"lang":"it","lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":2934512,"whitelabel":false,"cookiePolicyId":29463556, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-top-center","rejectButtonDisplay":true }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script> */
 }
