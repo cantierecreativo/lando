fc8439e2fe356a9d0fb70b4bbe60dbdff1170699
Guido Squillace <g.squillace@cantierecreativo.net>
2023-01-19 13:14:31 +0100

    Write exatcly banner of Iubenda

--- components/scripts/Iubenda.js
+++ components/scripts/Iubenda.js
@@ -9,36 +9,7 @@ export default function Iubenda({ siteId, policyId, visual, locale }) {
         id="iubenda"
         dangerouslySetInnerHTML={{
           __html: `var _iub = _iub || [];
-          _iub.csConfiguration = {
-            "askConsentAtCookiePolicyUpdate":true,
-            "countryDetection":true,
-            "enableLgpd":true,
-            "enableUspr":true,
-            "floatingPreferencesButtonDisplay":"bottom-right",
-            "invalidateConsentWithoutLog":true,
-            "lang":"${locale}",
-            "lgpdAppliesGlobally":false
-            "perPurposeConsent":true,
-            "purposes":"1,4,5",
-            "siteId":${siteId},
-            "cookiePolicyId":${policyId},
-            "banner":{
-              "acceptButtonDisplay":true,
-              "customizeButtonDisplay":true,
-              "position":"float-bottom-right",
-              "closeButtonDisplay":false,
-              "acceptButtonColor":"${buttonBackColor}",
-              "acceptButtonCaptionColor":"${buttonTextColor}",
-              "customizeButtonColor":"${buttonBackColor}",
-              "customizeButtonCaptionColor":"${buttonTextColor}",
-              "rejectButtonColor":"${buttonBackColor}",
-              "rejectButtonCaptionColor":"${buttonTextColor}",
-              "textColor":"${visual.colorText.hex}",
-              "backgroundColor":"${visual.colorBack.hex}",
-              "rejectButtonDisplay":true,
-              "closeButtonRejects":true
-            },
-            `,
+_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"countryDetection":true,"enableLgpd":true,"enableUspr":true,"floatingPreferencesButtonDisplay":"bottom-right","invalidateConsentWithoutLog":true,"lang":"it","lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":2934512,"whitelabel":false,"cookiePolicyId":29463556, "banner":{ "acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-top-center","rejectButtonDisplay":true }};`,
         }}
       />
       <Script id="iubenda-stub" src="//cdn.iubenda.com/cs/gpp/stub.js" />
