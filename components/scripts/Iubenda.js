ce5d33771db9fc1bfdaf5347361b99ae8248ea86
Guido Squillace <g.squillace@cantierecreativo.net>
2023-01-19 13:08:28 +0100

    Remove callback from Iubenda banner

--- components/scripts/Iubenda.js
+++ components/scripts/Iubenda.js
@@ -38,11 +38,6 @@ export default function Iubenda({ siteId, policyId, visual, locale }) {
               "rejectButtonDisplay":true,
               "closeButtonRejects":true
             },
-            "callback":{
-              onPreferenceExpressedOrNotNeeded: function(preference) {
-                window.consentIsGiven = preference;
-              }
-            }};
             `,
         }}
       />
