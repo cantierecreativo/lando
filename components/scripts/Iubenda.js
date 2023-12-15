e857c9402395859f45138cd5396a037d1434e049
Guido Squillace <info@guidosquillace.it>
2023-12-15 15:57:29 +0100

    GDPR upgrade

--- components/scripts/Iubenda.js
+++ components/scripts/Iubenda.js
@@ -1,39 +1,33 @@
 import Script from "next/script";
 
 export default function Iubenda({ siteId, policyId, visual, locale }) {
-  const buttonTextColor = visual.colorButton.hex
-  const buttonBackColor = visual.colorButtonBack.hex
+  const buttonTextColor = visual.colorButton.hex;
+  const buttonBackColor = visual.colorButtonBack.hex;
   return (
     <>
-      <Script
-        id="iubenda-policy"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
-        `}}
-      />
-      <Script
-        src="//cdn.iubenda.com/cs/iubenda_cs.js"
-        strategy="afterInteractive"
-      />
       <Script
         id="iubenda-cs"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `var _iub = _iub || [];
-            _iub.csConfiguration = {
-            "consentOnContinuedBrowsing": true,
-            "lang":"${locale}",
-            "siteId":${siteId},
-            "cookiePolicyId":${policyId},
+          __html: `
+          var _iub = _iub || [];
+          _iub.csConfiguration = {
+            "askConsentAtCookiePolicyUpdate": true,
+            "countryDetection": true,
+            "enableFadp": true,
+            "enableLgpd": true,
+            "lgpdAppliesGlobally": false,
             "perPurposeConsent": true,
-            "purposes": "1, 4, 5",
+            "siteId":${siteId},
+            "lang":"${locale}",
+            "cookiePolicyId":${policyId},
             "banner":{
               "acceptButtonDisplay":true,
-              "customizeButtonDisplay":true,
-              "position":"float-bottom-right",
               "closeButtonDisplay":false,
+              "customizeButtonDisplay":true,
+              "explicitWithdrawal":true,
+              "listPurposes":true,
+              "position":"bottom",
               "acceptButtonColor":"${buttonBackColor}",
               "acceptButtonCaptionColor":"${buttonTextColor}",
               "customizeButtonColor":"${buttonBackColor}",
@@ -43,16 +37,23 @@ export default function Iubenda({ siteId, policyId, visual, locale }) {
               "textColor":"${visual.colorText.hex}",
               "backgroundColor":"${visual.colorBack.hex}",
               "rejectButtonDisplay":true,
-              "closeButtonRejects":true
-            },
-            "callback":{
-              onPreferenceExpressedOrNotNeeded: function(preference) {
-                window.consentIsGiven = preference;
-              }
-            }};
-            `
+              "showPurposesToggles":true
+            }
+          }`,
         }}
       />
+      <Script
+        id="iubenda-script"
+        type="text/javascript"
+        strategy="afterInteractive"
+        src={`//cs.iubenda.com/sync/${siteId}.js`}
+      />
+      <Script
+        id="iubenda-cs"
+        type="text/javascript"
+        strategy="afterInteractive"
+        src="//cdn.iubenda.com/cs/beta/iubenda_cs.js"
+      />
     </>
-  )
+  );
 }
